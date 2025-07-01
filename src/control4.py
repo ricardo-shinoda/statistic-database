c83f9ae010230e170c115cf0efe5de67cc37ab65
ricardo-shinoda <ricardoshinoda@gmail.com>
2025-06-30 22:17:13 -0300

    updating

--- src/control4.py
+++ src/control4.py
@@ -60,7 +60,7 @@ if destination_file_path:
 
 
 #! rename this variable to save the file according to the invoice month
-month = "2025-03"
+month = "2025-06"
 
 # Read the CSV file with the specified delimiter
 df = pd.read_csv('invoice.csv', delimiter=';')
